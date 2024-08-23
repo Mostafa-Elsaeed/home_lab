479f523d1b59e7ea8dd30fb4f8efd7e686f557b1
Mostafa Elsaeed <98340604+Mostafa-Elsaeed@users.noreply.github.com>
2024-08-23 18:09:08 +0300

    Work Done: - remove unnececary volume in portianer compose - add docker pull imager script

---
+++ Raspberry PI/docker-image.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Pull Docker images
+docker pull portainer/portainer-ce:linux-arm
+docker pull adguard/adguardhome
+docker pull jc21/nginx-proxy-manager:latest
+docker pull strobi/rpi-gitea:latest
+docker pull jellyfin/jellyfin:latest
+docker pull fallenbagel/jellyseerr:latest
+docker pull lscr.io/linuxserver/prowlarr:latest
+docker pull linuxserver/radarr
+docker pull linuxserver/sonarr
+docker pull ghcr.io/linuxserver/bazarr
+docker pull linuxserver/deluge
+docker pull ghcr.io/ajnart/homarr:latest
+docker pull ghcr.io/alexta69/metube
+docker pull ghcr.io/meeb/tubesync:latest
+docker pull filebrowser
+docker pull ghcr.io/flaresolverr/flaresolverr:latest
+docker pull syncthing/syncthing
+docker pull ghcr.io/home-assistant/home-assistant:stable
+docker pull lscr.io/linuxserver/code-server:latest
+docker pull nextcloud
+docker pull postgres
+docker pull vaultwarden/server:latest
