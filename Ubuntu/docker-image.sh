a753241e10fc70bc5c504388931a7a1e9d225fec
Mostafa Elsaeed <98340604+Mostafa-Elsaeed@users.noreply.github.com>
2024-08-24 03:32:13 +0300

    Work Done: - update raspberry pi images

---
+++ Ubuntu/docker-image.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Pull Docker images
+docker pull portainer/portainer-ce:latest
+docker pull adguard/adguardhome:latest
+docker pull jc21/nginx-proxy-manager:latest
+docker pull gitea/gitea:latest
+docker pull jellyfin/jellyfin:latest
+docker pull fallenbagel/jellyseerr:latest
+docker pull lscr.io/linuxserver/prowlarr:latest
+docker pull linuxserver/radarr:latest
+docker pull linuxserver/sonarr:latest
+docker pull ghcr.io/linuxserver/bazarr
+docker pull linuxserver/deluge:latest
+docker pull ghcr.io/ajnart/homarr:latest
+docker pull ghcr.io/alexta69/metube:latest
+docker pull ghcr.io/meeb/tubesync:latest
+docker pull filebrowser/filebrowser:latest
+# docker pull ghcr.io/flaresolverr/flaresolverr:latest
+docker pull syncthing/syncthing:latest
+# docker pull ghcr.io/home-assistant/home-assistant:stable
+docker pull lscr.io/linuxserver/code-server:latest
+docker pull containrrr/watchtower:latest
+docker pull nextcloud:latest
+docker pull bitnami/redis:latest
+docker pull lscr.io/linuxserver/speedtest-tracker:latest
+# docker pull postgres:latest
+docker pull vaultwarden/server:latest
